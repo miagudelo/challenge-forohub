5f0161cd8a02ef98f27dc8ab607c585d3db4545a
miagudelo <162061470+miagudelo@users.noreply.github.com>
2024-07-11 07:12:11 -0500

    versión final Challenge ForoHub

---
+++ src/main/resources/db/migration/V3__inset-usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuario(id, nombre, correo_electronico, contrasena)
+VALUES(1, 'miagudelo', 'miagudelo@prueba.com', '$2y$10$SwulgT84Qciq4viFJEpZTewpq8Q3sQxMTWFXT1y/r04edj6ViBZ2m');
+
+INSERT INTO usuario(id, nombre, correo_electronico, contrasena)
+VALUES(2, 'mrestrepo', 'mrestrepo@prueba.com', '$2y$10$SwulgT84Qciq4viFJEpZTewpq8Q3sQxMTWFXT1y/r04edj6ViBZ2m');
---
+++ src/main/resources/db/migration/V4__insert-cursos.sql
@@ -0,0 +1,3 @@
+INSERT INTO forohub.curso VALUES(1, "Java","Programación");
+INSERT INTO forohub.curso VALUES(2, "CSS","Desarrollo Web");
+INSERT INTO forohub.curso VALUES(3, "HTML","Desarrollo Web");
